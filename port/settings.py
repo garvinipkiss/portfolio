37e31650ac87874bfd9921505a51c955c5f90c06
garvinipkiss <garvinipkiss@gmail.com>
2018-06-21 07:41:09 +0300

    heroku changes

--- port/settings.py
+++ port/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'portfolio',
+    'portfolio.apps.portfolio.Config'
     'tinymce',
     'bootstrap3',
     'django.contrib.admin',
@@ -125,7 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
